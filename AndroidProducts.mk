fa348027498305d9053b6f0b07f67efaab24989a
itsnouralawad <itsnouralwad@gmail.con>
2023-06-08 14:55:21 +0000

    merlin:Initial Make Files !

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+   $(LOCAL_DIR)/aosp_merlin.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_merlin-user \
+    aosp_merlin-userdebug \
+    aosp_merlin-eng
